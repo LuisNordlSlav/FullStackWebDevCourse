1e01f9353766c3f2466ce8c894bd0075ab1a6a15
theMagicMiner <johnnybellino@gmail.com>
2023-04-08 18:45:15 -0400

    Initial author Routes

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
@@ -22,7 +24,9 @@ db.once('open', () => console.log("connected to mongoose"))
 
 // routers
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
 
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 app.listen(process.env.PORT || 3000)
